737f5bafd15bdf4c2de007fdeea80541365e5d95
Poramee Chansuksett <poramee.chansuksett@gmail.com>
2019-10-26 17:42:27 +0700

    add ASK_Rx

--- Data Comm Lab09/9_1 ASK Rx/src/main.cpp
+++ Data Comm Lab09/9_1 ASK Rx/src/main.cpp
@@ -1,59 +1,67 @@
 #include <Adafruit_MCP4725.h>
 #include <Wire.h>
+Adafruit_MCP4725 dac;
 
-#define a0max
-#define a0min
-#define a0max 
-#define a0min
-#define a0max
-#define a1min 
-#define a1max
-#define a2min 
-#define a2max
-#define a3min 
-#define a3max
+#define defaultFreq 1700 // DAC SPEED
+/*freq0 : frequency of carrier sine wave (Hz) */
+#define freq0 700
+/* A[0] - A[3] : ASK Amplitude (0,5] (v) */
+const float A[4] = {1, 2, 3, 4};
+/*S_DAC : Amplitude (12bits) of sine wave at 0,90,180,270*/
+const uint16_t S_DAC[4] = {0, 90, 180, 270};
+int delay0;
+#define a0min 200
+#define a0max 300
+#define a1min 400
+#define a1max 550
+#define a2min 650
+#define a2max 800
+#define a3min 900
+#define a3max 1024
 
-#define r_slope
-
+#define r_slope 80
 int sum = 0;
-int max = 0;
+int m = 0;
 int prev = 0;
 int check = false;
 int output = -1;
 int count = 0;
 
 void setup() {
-  Seria.begin(115200);
+  // put your setup code here, to run once:
+  Serial.begin(115200);
+  dac.begin(0x64);
+  delay0 = (1000000 / freq0 - 1000000 / defaultFreq) / 4;
+  Serial.flush();
 }
 
 void loop() {
+  // put your main code here, to run repeatedly:
   int tmp = analogRead(A0);
-  if(tmp-prev > r_slope && check == false){
-    max = 0;
+  if (tmp - prev > r_slope && check == false) {
+    m = 0;
     check = true;
   }
-  if(tmp > max){
-    max = tmp;
+  if (tmp > m) {
+    m = tmp;
   }
-  if(max - tmp > r_slope){
-    if(check == true){
-      if(a0min < max && max < a0max){
+  if (m - tmp > r_slope) {
+    if (check == true) {
+      //      if(m > 10)Serial.println(m);
+      if (a0min < m && m < a0max) {
         Serial.print("0 0 ");
         count++;
-      }
-      else if(a0min < max && max < a1max){
+      } else if (a1min < m && m < a1max) {
         Serial.print("0 1 ");
-        count ++;
-      }
-      else if(a2min < max && max < a2max){
+        count++;
+      } else if (a2min < m && m < a2max) {
         Serial.print("1 0 ");
         count++;
-      }
-      else if(a3min < max && max < a3max){
+      } else if (a3min < m && m < a3max) {
         Serial.print("1 1 ");
         count++;
       }
-      if(count == 5){
+      if (count == 5) {
         Serial.println();
         count = 0;
       }
--- Data Comm Lab09/9_1 ASK Tx/src/main.cpp
+++ Data Comm Lab09/9_1 ASK Tx/src/main.cpp
@@ -12,21 +12,22 @@ char inData[20];
 
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   dac.begin(0x64);
   delay0 = (1000000/freq0 - 1000000/defaultFreq)/4;
   for(int i = 0;i < 4;++i){
     S_DAC[i] = (sin(zeta[i] * (PI / 180)) + 1) * (500);
-    Serial.println(S_DAC[i]);
+    // Serial.println(S_DAC[i]);
   }
   Serial.flush();
 }
 
 void loop() {
   if (Serial.available() > 0) {
+    Serial.println("-------");
     for (int i = 0; i < 20;i++) {
       inData[i] = Serial.read(); // Read a character
-      Serial.println(int(inData[i]));
+      // Serial.println(int(inData[i]));
     }
     /*use a cycle loop i for send data 8 bits*/
     for (int i = 0; inData[i] != -1; i++) {
@@ -45,10 +46,13 @@ void loop() {
               Use the selected amplitude above to modify sine amplitude
             */
             dac.setVoltage(S_DAC[sl] * A[tmp], false);
-            Serial.println(S_DAC[sl] * A[tmp]);
+            // Serial.println(S_DAC[sl] * A[tmp]);
             delayMicroseconds(delay0); // sampling delay
           }
         }
+        Serial.print((tmp >> 1 )&1);
+        Serial.print(" ");
+        Serial.println((tmp)&1);
         inData[i] >>= 2;
       }
     }
